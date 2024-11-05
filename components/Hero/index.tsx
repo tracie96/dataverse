0821a7eb34d07c7825926130f57b7a0c5071d903
Tracy <tracy.anele@pictureworks.com.au>
2024-11-05 14:32:10 +0100

    update

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -26,7 +26,7 @@ const Hero = () => {
 
   return (
     <>
-      <section className="overflow-hidden pb-20 pt-35 md:pt-40 xl:pb-25 xl:pt-46">
+      <section className="overflow-hidden pt-35 md:pt-40  xl:pt-46">
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
             <div className=" md:w-1/2">
