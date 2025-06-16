773778c09a27902ed41b56739af98d675d41902d
tracie96 <tracyamara07@gmail.com>
2025-06-16 19:59:25 +0100

    update

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("1897dcdd55ed8076")
+/******/ 		__webpack_require__.h = () => ("99a4fb03fbfccdb4")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/(site)/applications/page.tsx
+++ app/(site)/applications/page.tsx
@@ -167,7 +167,7 @@ export default function ApplicationsTable() {
                         <div>
                             <button
                                 type="submit"
-                                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                             >
                                 Sign in
                             </button>
