7cf4a002f51d899700fc982b9f7c1d56358d2d50
tracie96 <tracyamara07@gmail.com>
2025-09-04 16:06:31 +0100

    update

--- components/Payment/StripePayment.tsx
+++ components/Payment/StripePayment.tsx
@@ -227,27 +227,7 @@ const StripePayment = ({ amount, currency, onSuccess, onError, applicationData }
 
   return (
     <div className="space-y-4">
-      {/* Test Mode Warning */}
-      <div className="bg-yellow-50 dark:bg-yellow-900/20 border border-yellow-200 dark:border-yellow-800 rounded-lg p-4">
-        <div className="flex items-start gap-3">
-          <div className="text-yellow-600 dark:text-yellow-400 mt-0.5">
-            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-            </svg>
-          </div>
-          <div>
-            <h4 className="font-medium text-yellow-800 dark:text-yellow-200 mb-1">
-              Payment Mode Notice
-            </h4>
-            <p className="text-sm text-yellow-700 dark:text-yellow-300">
-              {process.env.NODE_ENV === 'development' 
-                ? "You're in development mode. Use test card numbers like 4242 4242 4242 4242."
-                : "You're in production mode. Please use real credit/debit cards only."
-              }
-            </p>
-          </div>
-        </div>
-      </div>
+    
 
       <Elements stripe={stripePromise} options={{ 
         clientSecret,
